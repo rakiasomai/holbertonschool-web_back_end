472b4403490a29307cbb5565b346b6fa1cb533b3
rakiasomai <1069@holbertonschool.com>
2020-10-19 15:32:37 +0000

    task6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' annotations '''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    ''' Function that return the sum of the list '''
+    return sum(mxd_lst)
