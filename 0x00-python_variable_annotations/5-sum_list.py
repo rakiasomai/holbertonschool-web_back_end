ebaf7d0292cf8111355e91dd7864c1f4ef914600
rakiasomai <1069@holbertonschool.com>
2020-10-19 15:29:10 +0000

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' annotations '''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    ''' Funtion that return the sum of the list '''
+    return sum(input_list)
+
