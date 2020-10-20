3cfe681b8c38e9a409316fc3effa3c05aca5428a
rakiasomai <1069@holbertonschool.com>
2020-10-20 21:29:29 +0000

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,6 +11,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     ''' Function that returns the list
     '''
-    l = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    l = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(l)]
     return finish
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,6 +10,6 @@ get = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ''' Function that returns a list
     '''
-    l = [get(max_delay) for _ in range(n)]
+    l = [get(max_delay) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(l)]
     return finish
