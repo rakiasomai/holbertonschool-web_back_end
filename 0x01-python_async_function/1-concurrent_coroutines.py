496f9337d6d0bc623c0c1684da672edb17e6d1c0
rakiasomai <1069@holbertonschool.com>
2020-10-20 21:34:44 +0000

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' async and await syntax '''
+""" async and await syntax """
 import asyncio
 from typing import List
 import random
@@ -9,8 +9,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    ''' Function that returns the list
-    '''
+    """ Function that returns the list
+    """
     l = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(l)]
     return finish
