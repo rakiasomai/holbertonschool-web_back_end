aa1de14b1e93c689cfa426234858659fd19fd617
rakiasomai <1069@holbertonschool.com>
2020-10-26 22:21:39 +0000

    task1

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 ''' caching systems '''
+from base_caching import BaseCaching
 
-BaseCaching = __import__("base_caching").BaseCaching
+
+class FIFOCache(BaseCaching):
+    ''' FIFOcache '''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        ''' Function put '''
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            x = sorted(self.cache_data)[0]
+            self.cache_data.pop(x)
+            print('DISCARD:', x)
+
+    def get(self, key):
+        ''' Function get '''
+        if key and key in self.cache_data:
+            return self.cache_data.get(key)
+        else:
+            return None
