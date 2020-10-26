3999f59c2d1c46f1b0091277dde627ee95359def
rakiasomai <1069@holbertonschool.com>
2020-10-26 20:51:25 +0000

    fix

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -4,18 +4,18 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache class inherit from BaseCaching"""
+    ''' BasicCache class inherit from BaseCaching '''
+
+    def get(self, key):
+        ''' function get
+        '''
+        if key is None or key not in self.cache_data.keys():
+            return
+        return self.cache_data.get(key)
 
     def put(self, key, item):
-        ''' puts
+        ''' function put
         '''
         if key is None or item is None:
             return
         self.cache_data.update({key: item})
-
-     def get(self, key):
-        ''' Gets
-        '''
-        if key is None or key not in self.cache_data.keys():
-            return
-        return self.cache_data.get[key]
