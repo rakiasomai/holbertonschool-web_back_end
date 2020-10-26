aafbe4b5b3e8186a5c7384363659118676f764b4
rakiasomai <1069@holbertonschool.com>
2020-10-26 20:30:56 +0000

    first

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' caching systems '''
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' BassicCache '''
+
+    def put(self, key, item):
+        ''' Function put '''
+        pass
+
+    def get(self, key):
+        ''' Function get '''
+        pass
