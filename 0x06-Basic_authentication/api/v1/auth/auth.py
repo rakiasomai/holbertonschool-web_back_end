72fe05064f572b3dd80b6b91832582fa2333841b
rakiasomai <somai.rakia@hotmail.fr>
2021-01-02 13:20:40 +0100

    task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,18 @@ class Auth():
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' require_authentification '''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path is None or excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' def authorization_header '''
