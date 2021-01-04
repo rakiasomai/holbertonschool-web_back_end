a1cf6d4e9d4fcf9fdb4873bacc30f205767d1395
rakiasomai <somai.rakia@hotmail.fr>
2021-01-04 19:48:23 +0100

    .

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -16,6 +17,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Module of Basic_auth
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    ''' BasicAuth class
+    '''
+    pass
