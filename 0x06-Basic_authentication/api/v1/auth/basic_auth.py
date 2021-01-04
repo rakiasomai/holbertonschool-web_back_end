88ef8823e381cd6e9046890c94cbe991c77f8d13
rakiasomai <somai.rakia@hotmail.fr>
2021-01-04 20:12:12 +0100

    .

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     ''' BasicAuth class
     '''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        ''' def extract base64 authorization header '''
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith("Basic "):
+            return "".join(authorization_header.split(" ")[1:])
