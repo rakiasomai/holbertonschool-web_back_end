18eec69b0a88213063fc7ee44c8f6392c5b5cc79
rakiasomai <somai.rakia@hotmail.fr>
2021-01-05 13:59:04 +0100

    .

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' Module of Basic_auth
 '''
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -15,3 +16,13 @@ class BasicAuth(Auth):
             return None
         if authorization_header.startswith("Basic "):
             return "".join(authorization_header.split(" ")[1:])
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        ''' def decode base 64 authorization '''
+        if base64_authorization_header and type(base64_authorization_header) == str:
+             try:
+                 x = base64_authorization_header.encode('utf-8')
+                 base = base64.b64decode(x)
+                 return base.decode('utf-8')
+             except Exception:
+                 return None
