7af0f7bbff4f44689346bc711940448236f26291
rakiasomai <somai.rakia@hotmail.fr>
2021-01-06 14:43:08 +0100

    fix

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale():
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world():
     ''' return the template '''
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
