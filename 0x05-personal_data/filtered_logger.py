f3c6d313b1b005e1d5b1fb5258f8afe9618776fa
rakiasomai <somai.rakia@hotmail.fr>
2021-01-09 13:07:18 +0100

    fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,6 +9,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     ''' Returns the log message obfuscated '''
     for field in fields:
-        msg = re.sub(rf"{field}=(.*?)\{separator}",
+        message = re.sub(rf"{field}=(.*?)\{separator}",
                      f'{field}={redaction}{separator}', message)
-    return msg
+    return message
