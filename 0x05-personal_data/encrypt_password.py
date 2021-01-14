d0451ba7f377b7f352c66cdb3b3ee366c2ca906d
rakiasomai <somai.rakia@hotmail.fr>
2021-01-14 20:08:21 +0100

    .

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,4 +6,11 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     ''' def hash password '''
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    var = password.encode('utf-8')
+    return bcrypt.hashpw(var, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' def is valid '''
+    var = password.encode('utf-8')
+    return bcrypt.checkpw(var, hashed_password)
