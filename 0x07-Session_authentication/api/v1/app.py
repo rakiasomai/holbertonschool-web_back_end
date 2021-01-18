2a676b539bcb32fb2742398ff7d2bea5dbc3ca95
rakiasomai <somai.rakia@hotmail.fr>
2021-01-18 14:46:13 +0100

    first

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -28,12 +28,15 @@ def before_request():
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
     if auth is None:
-        return
-    if not auth.require_auth(request.path, excluded_paths):
-        return
+        return None
+    if auth.require_auth(request.path, excluded_paths):
+        pass
+    else:
+        return None
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
         abort(403)
 
 
