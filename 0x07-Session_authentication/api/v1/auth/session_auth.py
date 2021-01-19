5a1c35a71d1cf85899e83e221b5cb51819001a19
rakiasomai <somai.rakia@hotmail.fr>
2021-01-20 00:12:53 +0100

    task3

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,11 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        ''' def user id for session id '''
+        if session_id is None:
+            return None
+        if type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
