ca2d7dfe1fd749cf2b112e1116f2f018b2c9017a
rakiasomai <somai.rakia@hotmail.fr>
2021-01-19 15:02:40 +0100

    task1

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -19,6 +20,8 @@ if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+if os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Session Auth '''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    ''' class auth '''
+    pass
