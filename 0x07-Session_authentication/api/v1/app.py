6891ed296907423ad2ad2eb2f97f0e226aa4c0ce
rakiasomai <somai.rakia@hotmail.fr>
2021-01-20 15:23:17 +0100

    fix task5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -30,16 +30,10 @@ def before_request():
     """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if auth is None:
-        return None
-    if auth.require_auth(request.path, excluded_paths):
-        pass
-    else:
-        return None
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
-        abort(401)
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
     request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
