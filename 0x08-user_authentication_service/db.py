10c6696ce12651d949a362598346c79816ee33ff
rakiasomai <somai.rakia@hotmail.fr>
2021-01-25 14:25:14 +0100

    task1

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+''' Module data base '''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        ''' def add user '''
+        n_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(n_user)
+        self._session.commit()
+        return n_user
+    
