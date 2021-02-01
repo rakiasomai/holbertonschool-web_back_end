d54f84e615610120267fe976ac978cb6987a2651
rakiasomai <somai.rakia@hotmail.fr>
2021-02-01 16:15:31 +0100

    task16

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -67,3 +67,15 @@ class Auth:
         ''' def destroy session '''
         if user_id:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        ''' def get reset password token '''
+        if email:
+            try:
+                user = self._db.find_user_by(email=email)
+            except NoResultFound:
+                raise ValueError
+            else:
+                take = _generate_uuid()
+                self._db.update_user(user.id, reset_token=take)
+                return take
