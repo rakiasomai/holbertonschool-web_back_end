3c174129609cf8b6b48d0954a15fab13f716cfa7
rakiasomai <somai.rakia@hotmail.fr>
2021-02-01 16:40:10 +0100

    task18

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -84,9 +84,8 @@ class Auth:
         ''' def update password '''
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_pass = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_pass,
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
                                  reset_token=None)
-            return None
-        except Exception:
+        except NoResultFound:
             raise ValueError
