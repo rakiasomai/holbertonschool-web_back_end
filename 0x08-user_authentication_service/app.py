71be5d3de83b5a5de93a3314906e9d61781075be
rakiasomai <somai.rakia@hotmail.fr>
2021-02-01 14:11:54 +0100

    task6

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -14,5 +14,17 @@ def message():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    ''' def users '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
