1c814e1f7a7b5757e1533e2898abc68462097b48
rakiasomai <somai.rakia@hotmail.fr>
2021-02-01 14:35:36 +0100

    task10

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -43,3 +43,13 @@ class Auth:
                 return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        ''' def create session '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
