a8a2d05c7041051a47b5557006a26f73f91fb220
rakiasomai <somai.rakia@hotmail.fr>
2021-02-03 11:39:48 +0100

    task1

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM ('TN', 'CO', 'US') NOT NULL
+    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
