2652ad6d1253df6b96a8a7658fe8f382944ed11a
rakiasomai <somai.rakia@hotmail.fr>
2021-02-03 13:21:11 +0100

    task11

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- sql project
+CREATE VIEW need_meeting AS
+SELECT 
+    name
+FROM 
+    students
+WHERE (score < 80) AND (last_meeting IS NULL) OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
