e9c926a60d2a7735ca0cfa3aefff54a7c8dd2f2e
rakiasomai <somai.rakia@hotmail.fr>
2021-02-04 12:53:57 +0100

    task12

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+''' MongoDB '''
+from pymongo import MongoClient
+
+
+def log_stats():
+    ''' def log stats '''
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    data = client.logs.nginx
+    print("{} logs".format(data.count_documents({})))
+    print('Methods:')
+
+    for y in methods:
+        print("\ty {}: {}".format(
+                y, data.count_documents({'method': y})))
+
+    print("{} status check".format(
+        data.count_documents({'method': 'GET', 'path': '/status'})))
+
+if __name__ == "__main__":
+    log_stats()
