f5aeeda56def93fe13d901dd52217b0dbd4124e9
rakiasomai <somai.rakia@hotmail.fr>
2021-02-06 23:17:04 +0100

    task1

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(student) {
-    if (Array.isArray(student) == false) {
+  if (Array.isArray(student) == false) {
     return [];
     }
-    return student.map((l_student) => (l_student.id));
+  return student.map((l_student) => (l_student.id));
 }
